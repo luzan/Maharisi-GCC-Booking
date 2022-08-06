a747a364e5e141f995f92bc98ef4da087467b997
Luzan Baral <luzanbaral@gmail.com>
2022-08-05 22:30:54 -0500

    cors issue fixed for login

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 require('dotenv').config()
 
 mongoose.connect(`mongodb+srv://miuBooking:${process.env.MONGO_PWD}@cluster0.ujquu0n.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1/users', require('./routers/users'));
 app.use('/api/v1/rooms', require('./routers/rooms'));
