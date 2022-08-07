621f861138ba48b370b991d4045c6ab05550e928
Luzan Baral <luzanbaral@gmail.com>
2022-08-07 13:55:54 -0500

    Role based authorization implemented for all the routes

--- backend/routers/bookings.js
+++ backend/routers/bookings.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { getAllBookings, getBookingById, createBooking, updateBooking, deleteBooking } = require('../controllers/bookingController');
 const checkToken = require('../middlewares/checkToken');
+const authorize = require('../middlewares/authorize');
+const Role = require('../_helpers/roles');
+
 // api/v1/bookings
 
-router.get('/', checkToken, getAllBookings);
-router.get('/:id', checkToken, getBookingById);
-router.post('/', checkToken, createBooking);
-router.put('/:id', checkToken, updateBooking);
-router.delete('/:id', checkToken, deleteBooking);
+router.get('/', checkToken, authorize(Role.Admin), getAllBookings);
+router.get('/:id', checkToken, authorize(), getBookingById);
+router.post('/', checkToken, authorize(), createBooking);
+router.put('/:user_id/:booking_id', checkToken, authorize(), updateBooking);
+router.delete('/:id', checkToken, authorize(Role.Admin), deleteBooking);
 
 module.exports = router;
--- backend/routers/payments.js
+++ backend/routers/payments.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { getAllPayments, getPaymentById, createPaymentForBooking } = require('../controllers/paymentController');
+const authorize = require('../middlewares/authorize');
+const checkToken = require('../middlewares/checkToken');
+const Role = require('../_helpers/roles');
 // /api/v1/payments
 
-router.get('/', getAllPayments);
-router.get('/:id', getPaymentById);
-router.post('/booking/:booking_id', createPaymentForBooking);
+router.get('/', checkToken, authorize(Role.Admin), getAllPayments);
+router.get('/:id', checkToken, authorize(), getPaymentById);
+router.post('/booking/:booking_id', checkToken, authorize(), createPaymentForBooking);
 // router.put('/:id', updatePayment);
 // router.delete('/:id', deletePayment);
 
