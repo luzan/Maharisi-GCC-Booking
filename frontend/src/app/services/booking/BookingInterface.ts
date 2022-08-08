eb4660b63edda19f459f4c5cf7838620db09ac97
Luzan Baral <luzanbaral@gmail.com>
2022-08-07 22:20:14 -0500

    updated booking interface

--- frontend/src/app/services/booking/BookingInterface.ts
+++ frontend/src/app/services/booking/BookingInterface.ts
@@ -1,21 +1,21 @@
 export interface Booking {
     _id?: string;
+    userId?: string;
     firstName: string;
     middleName: string;
     lastName: string;
     email: string;
-    phoneNumber: number;  
-      
-    staff: string; 
-    student: string;
-    guest: string; 
+    phoneNumber: number;
+
+    bookingFor: string;
+    roomType: string;
 
     checkOutDate: number;
     checkInDate: number;
 
     purposeOfStay: string;
-    accessibleRequired: string;
-    occcupants: string;
+    accessibleRequired: boolean;
+    occcupants: number;
 
     arrivalTime: number;
     numberOfGuests: number;
