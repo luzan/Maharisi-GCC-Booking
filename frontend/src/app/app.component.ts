5755f5b4fb216f5e4ade39af213f0b64063b7e26
Luzan Baral <luzanbaral@gmail.com>
2022-08-07 22:22:57 -0500

    fixed redirection based on role

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './services/user/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,16 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'MIU GCC Booking System';
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.refreshState();
+    const userState = this.userService.getUserState();
+    if (userState?.role === 'admin') {
+      this.router.navigate(['/dashboard']);
+    } else if (userState?.role === 'user') {
+      this.router.navigate(['/booking']);
+    } else {
+      this.userService.logout();
+      this.router.navigate(['/login']);
+    }
+  }
 }
