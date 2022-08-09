eda52139f19a0ea80ea076a11468f0095a143796
sreytouch <atouch.it@gmail.com>
2022-08-09 00:13:23 -0500

    add room with api

--- frontend/src/app/services/room/room.service.ts
+++ frontend/src/app/services/room/room.service.ts
@@ -1,16 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Room } from './roomInterface';
+import jwt_decode from "jwt-decode";
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
+  roomState$ = new BehaviorSubject<{ token: string }>({ token: '' });
 
   constructor(private http: HttpClient) { }
 
   getRooms(query: string): any {
     return this.http.get<any>(`${environment.apiUrl}/rooms/?${query}`);
+  }
+
+  getRoomById(room_id?: string): any {
+    return this.http.get<any>(`${environment.apiUrl}/rooms/${room_id}`);
+  }
+
+  addRoom(room: Room) {
+    return this.http.post<any>(`${environment.apiUrl}/rooms`, room);
+  }
+
+  updateRoomBooking(room: Room, room_id?: string): any {
+    return this.http.put(`${environment.apiUrl}/rooms/${room_id}`, room);
   }
 
 }
