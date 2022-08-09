57d39aa3f6f4cd717e3c13a8b6718a91f3fc72d7
Luzan Baral <luzanbaral@gmail.com>
2022-08-09 10:58:48 -0500

    payment service initialized with getAllPayment

---
+++ frontend/src/app/services/payment/payment.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllPaymentData(): any {
+    return this.http.get(`${environment.apiUrl}/payments`);
+  }
+}
